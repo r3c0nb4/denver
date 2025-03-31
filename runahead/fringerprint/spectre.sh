506cdd6a120a77c8bad88be4fd54f1fb26a23095
r3c0nb4 <r3c0n11@protonmail.com>
2025-03-31 20:35:05 +0200

    fringerprint runahead execution

---
+++ runahead/fringerprint/spectre.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+count=120
+for i in {1..5}
+do	
+	make clean
+	taskset -c 2 ./whatever
+	K=$((i % 10))
+    make CFLAGS="-DN=${K}"
+
+	taskset -c 2 ../bin/spectre
+#	taskset -c 1 ../bin/spectre >> ./correctness/res.txt
+	
+	count=$((count + 1))
+done
+
+
